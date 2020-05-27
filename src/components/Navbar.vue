c515facc294390b345d2cb3c3ba5e6f44b44651f
ivanawetek <ivana.kovacevic@wetek.com>
2020-05-27 17:42:28 +0200

    student and bnavbar components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="success">
+    <b-navbar-nav>
+      <b-nav-text>STUDENT LIST</b-nav-text>
+    </b-navbar-nav>
+</b-navbar>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
---
+++ src/components/Student/SudentList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>This is student-list component page.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'StudentList',
+  data() {
+    return {
+      students: []
+    };
+  }
+};
+</script>
