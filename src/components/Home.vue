7aa6892c1d8fb5316911de4019ba63c51a8e5fb0
ivanawetek <ivana.kovacevic@wetek.com>
2020-05-27 14:02:54 +0200

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="homeText">
+    WELCOME TO STUDENT APPLICATION
+  </p>
+</template>
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+<style scoped>
+  .homeText {
+    font-size: 35px;
+    color: rebeccapurple;
+    text-align: center;
+    position: relative;
+    top: 30px;
+    text-shadow: 2px 2px 2px gray;
+  }
+</style>
