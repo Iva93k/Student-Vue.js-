02235a8f02181412314dc3106b5be3b6153150d7
ivanawetek <ivana.kovacevic@wetek.com>
2020-05-27 19:15:29 +0200

    Creating component props and events

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,69 +1,75 @@
 <template>
-    <div>
-      <b-row>
-        <b-col md="2"
-               offset-md="10">
-          <a href="#">Create student</a>
-        </b-col>
-      </b-row>
-      <br>
-      <b-row>
-        <b-col md="12">
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Index number</th>
-                  <th>First name</th>
-                  <th>Last name</th>
-                  <th>Year</th>
-                  <th>Status Id</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="student in students"
-                    :key="student.pkstudentId">
-                  <td>{{ student.pkstudentId }}</td>
-                  <td>{{ student.indexNumber }}</td>
-                  <td>{{ student.firstName }}</td>
-                  <td>{{ student.lastName }}</td>
-                  <td>{{ student.year }}</td>
-                  <td>{{ student.studentStatusId }}</td>
-                  <td>{{ student.studentStatus }}</td>
-                  <td>
-                    <b-button variant="default">Details</b-button>
-                  </td>
-                  <td>
-                    <b-button variant="success">Update</b-button>
-                  </td>
-                  <td>
-                    <b-button variant="danger">Delete</b-button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </b-col>
-      </b-row>
-    </div>
-  </template>
-  <script>
-    import StudentService from '@/api-services/student.service';
-    export default {
-      name: 'Home',
-      data() {
-        return {
-          students: []
-        };
+  <div>
+    <b-row>
+      <b-col md="2"
+             offset-md="10">
+        <a href="#">Create student</a>
+      </b-col>
+    </b-row>
+    <br>
+    <b-row>
+      <b-col md="12">
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Index number</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Year</th>
+                <th>Status Id</th>
+                <th>Status</th>
+                <th>Details</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <student-list-row
+                v-for="student in students"
+                :key="student.pkstudentId"
+                :student="student"
+                @details="detailsStudent"
+                @update="updateStudent"
+                @delete="deleteStudent"/>
+            </tbody>
+          </table>
+        </div>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+  import StudentService from '@/api-services/student.service';
+  import StudentListRow from '@/components/Student/StudentListRow';
+
+  export default {
+    name: 'Home',
+    components: {
+      StudentListRow
+    },
+    data() {
+      return {
+        students: []
+      };
+    },
+    created() {
+      StudentService.getAll().then((response) => {
+        this.students = response.data;
+      });
+    },
+    methods: {
+      detailsStudent(pkstudentId) {
+        console.log('details', pkstudentId);
       },
-      created() {
-        StudentService.getAll().then((response) => {
-          this.students = response.data;
-        });
+      updateStudent(pkstudentId) {
+        console.log('update', pkstudentId);
+      },
+      deleteStudent(pkstudentId) {
+        console.log('delete', pkstudentId);
       }
-    };
-  </script>
-  <style scoped>
-  </style>
+    }
+  };
+</script>
+
---
+++ src/components/Student/StudentListRow.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr>
+    <td>{{ student.pkstudentId }}</td>
+    <td>{{ student.indexNumber }}</td>
+    <td>{{ student.firstName }}</td>
+    <td>{{ student.lastName }}</td>
+    <td>{{ student.year }}</td>
+    <td>{{ student.studentStatusId }}</td>
+    <td>{{ student.studentStatus }}</td>
+    <td>
+      <b-button
+        variant="default"
+        @click="onDetailsClick">Details</b-button>
+    </td>
+    <td>
+      <b-button
+        variant="success"
+        @click="onUpdateClick">Update</b-button>
+    </td>
+    <td>
+      <b-button
+        variant="danger"
+        @click="onDeleteClick">Delete</b-button>
+    </td>
+  </tr>
+</template>
+<script>
+export default {
+  name: 'StudentListRow',
+    props: ['student'],
+    methods: {
+      onDetailsClick() {
+        this.$emit('details', this.student.pkstudentId);
+      },
+      onUpdateClick() {
+        this.$emit('update', this.student.pkstudentId);
+      },
+      onDeleteClick() {
+        this.$emit('delete', this.student.pkstudentId);
+      }
+    }
+};
+</script>
